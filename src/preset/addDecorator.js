9e097c813ad116a60f37f36cc5a8df5a422dcfef
winkerVSbecks <varunvachhar@gmail.com>
2021-06-25 20:55:54 -0400

    Add default for the outline global value

--- src/preset/addDecorator.js
+++ src/preset/addDecorator.js
@@ -1,3 +1,8 @@
 import { withOutline } from '../withOutline';
+import { PARAM_KEY } from '../constants';
 
 export const decorators = [withOutline];
+
+export const globals = {
+  [PARAM_KEY]: false,
+};
